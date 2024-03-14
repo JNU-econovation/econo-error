766b2a91d8f77c2ac40dc20deb57fc6dfb4f1da8
hai <xgkg0344@naver.com>
2024-03-14 21:02:29 +0900

    feat: fullcalendar 설치

--- FE/error/src/App.jsx
+++ FE/error/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 function App() {
-  return <></>;
+  return (
+    <>
+      {" "}
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+    </>
+  );
 }
 
 export default App;
