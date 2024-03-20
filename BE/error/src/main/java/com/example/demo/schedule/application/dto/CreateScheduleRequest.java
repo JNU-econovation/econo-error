cb7293f664d81c32b88bc1b3ee63cfe0163e6e05
capDoYeonLee <ghtyu21@naver.com>
2024-03-20 21:22:12 +0900

    feat: 일정 생성 요청 dto 추가

---
+++ BE/error/src/main/java/com/example/demo/schedule/application/dto/CreateScheduleRequest.java
@@ -0,0 +1,22 @@
+package com.example.demo.schedule.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class CreateScheduleRequest {
+
+    private String eventName;
+    private LocalDateTime eventStartDate;
+    private LocalDateTime eventEndDate;
+    private String eventInfo;
+    private String eventPlace;
+
+}
