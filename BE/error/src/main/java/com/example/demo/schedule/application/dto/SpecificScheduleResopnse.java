c8eef2d855945ff105893a294947171ccfa41212
capDoYeonLee <ghtyu21@naver.com>
2024-03-20 21:26:00 +0900

    feat: 특정 일정 조회 응답 dto 추가

---
+++ BE/error/src/main/java/com/example/demo/schedule/application/dto/SpecificScheduleResopnse.java
@@ -0,0 +1,20 @@
+package com.example.demo.schedule.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class SpecificScheduleResopnse {
+    private Long eventId;
+    private String eventName;
+    private String eventInfo;
+    private LocalDateTime eventStartDate;
+    private LocalDateTime eventEndDate;
+}
