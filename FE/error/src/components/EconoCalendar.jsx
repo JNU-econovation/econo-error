b1fbfc4970621bb9fe02fc78ef01e66eddf97752
hai <xgkg0344@naver.com>
2024-03-27 01:43:39 +0900

    style: 달력 크기 및 배치

--- FE/error/src/components/EconoCalendar.jsx
+++ FE/error/src/components/EconoCalendar.jsx
@@ -1,12 +1,24 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import styled from "styled-components";
+import { Calendar } from "@fullcalendar/core";
 const EconoCalendar = () => {
   return (
     <>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <CalendarContainer>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          height={"54.64rem"}
+        />
+      </CalendarContainer>
     </>
   );
 };
 
 export default EconoCalendar;
+
+const CalendarContainer = styled.div`
+  margin-left: 17.7rem;
+  margin-top: 1.5rem;
+`;
