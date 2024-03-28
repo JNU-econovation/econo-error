f419529d567be655aeb3c30aa2abe26c39e84fa1
capDoYeonLee <ghtyu21@naver.com>
2024-03-28 15:28:57 +0900

    fix: JpaRepositroy 설정 수정

--- BE/error/src/main/java/com/example/demo/ErrorApplication.java
+++ BE/error/src/main/java/com/example/demo/ErrorApplication.java
@@ -3,8 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class})
+@EnableJpaRepositories
+@SpringBootApplication
+@EnableJpaAuditing
 public class ErrorApplication {
 
 	public static void main(String[] args) {
