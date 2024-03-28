9dfdc69ba03829c0a546ff4fb9e904a16307383d
capDoYeonLee <ghtyu21@naver.com>
2024-03-28 11:43:57 +0900

    fix: reopsitory 어노테이션 추가

--- BE/error/src/main/java/com/example/demo/schedule/persistence/ScheduleRepository.java
+++ BE/error/src/main/java/com/example/demo/schedule/persistence/ScheduleRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo.schedule.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
 }
