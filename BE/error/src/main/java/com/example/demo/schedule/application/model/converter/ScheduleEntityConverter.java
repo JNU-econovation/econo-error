6f1efb81c23f8800070b4aeb649e2d5ac5a3e7cc
capDoYeonLee <ghtyu21@naver.com>
2024-03-28 15:31:29 +0900

    feat: method from override

--- BE/error/src/main/java/com/example/demo/schedule/application/model/converter/ScheduleEntityConverter.java
+++ BE/error/src/main/java/com/example/demo/schedule/application/model/converter/ScheduleEntityConverter.java
@@ -9,14 +9,21 @@ import org.springframework.stereotype.Component;
 public class ScheduleEntityConverter implements AbstractEntityConverter<ScheduleEntity, ScheduleModel> {
 
     @Override
-    public ScheduleModel from(ScheduleEntity scheduleEntity) {
-        return null;
+    public ScheduleModel from(ScheduleEntity source) {
+        return ScheduleModel.builder()
+                .eventId(source.getEventId())
+                .eventName(source.getEventName())
+                .eventStartDate(source.getEventStartDate())
+                .eventEndDate(source.getEventEndDate())
+                .eventPlace(source.getEventPlace())
+                .eventInfo(source.getEventInfo())
+                .build();
     }
 
     @Override
     public ScheduleEntity toEntity(ScheduleModel source) {
         return ScheduleEntity.builder()
-                .eventId(source.getId())
+                .eventId(source.getEventId())
                 .eventName(source.getEventName())
                 .eventStartDate(source.getEventStartDate())
                 .eventEndDate(source.getEventEndDate())
