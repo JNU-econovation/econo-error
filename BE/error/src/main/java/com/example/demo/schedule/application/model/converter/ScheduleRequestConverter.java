2bbff6b3897de3919c330cf4c1b15444f3564c93
capDoYeonLee <ghtyu21@naver.com>
2024-03-28 11:38:06 +0900

    feat: Schedule Reqeust Converter 기능 개발

---
+++ BE/error/src/main/java/com/example/demo/schedule/application/model/converter/ScheduleRequestConverter.java
@@ -0,0 +1,30 @@
+package com.example.demo.schedule.application.model.converter;
+
+import com.example.demo.common.support.converter.AbstractDtoConverter;
+import com.example.demo.schedule.application.dto.CreateScheduleRequest;
+import com.example.demo.schedule.application.model.ScheduleModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleRequestConverter implements AbstractDtoConverter<CreateScheduleRequest, ScheduleModel> {
+
+    @Override
+    public ScheduleModel from(CreateScheduleRequest source) {
+        return ScheduleModel.builder()
+                .eventName(source.getEventName())
+                .eventStartDate(source.getEventStartDate())
+                .eventEndDate(source.getEventEndDate())
+                .eventInfo(source.getEventInfo())
+                .eventPlace(source.getEventPlace())
+                .build();
+    }
+    /*public ScheduleModel from(CreateScheduleRequest request) {
+        return ScheduleModel.builder()
+                .eventName(request.getEventName())
+                .eventStartDate(request.getEventStartDate())
+                .eventEndDate(request.getEventEndDate())
+                .eventInfo(request.getEventInfo())
+                .eventPlace(request.getEventPlace())
+                .build();
+    }*/
+}
