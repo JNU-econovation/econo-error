e036170a5b9b6191e2804789c96ad6e4b513ff3c
capDoYeonLee <ghtyu21@naver.com>
2024-03-28 11:56:29 +0900

    feat: 환경세팅 수정

--- BE/error/src/main/java/com/example/demo/ErrorApplication.java
+++ BE/error/src/main/java/com/example/demo/ErrorApplication.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class})
 public class ErrorApplication {
 
 	public static void main(String[] args) {
