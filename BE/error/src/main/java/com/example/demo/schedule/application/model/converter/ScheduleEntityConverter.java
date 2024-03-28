4e76279e4cb78692b30920ea9c13dd18fa1114f3
capDoYeonLee <ghtyu21@naver.com>
2024-03-28 11:37:19 +0900

    feat: Schedule entity converter 기능 개발

---
+++ BE/error/src/main/java/com/example/demo/schedule/application/model/converter/ScheduleEntityConverter.java
@@ -0,0 +1,27 @@
+package com.example.demo.schedule.application.model.converter;
+
+import com.example.demo.common.support.converter.AbstractEntityConverter;
+import com.example.demo.schedule.application.model.ScheduleModel;
+import com.example.demo.schedule.persistence.ScheduleEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleEntityConverter implements AbstractEntityConverter<ScheduleEntity, ScheduleModel> {
+
+    @Override
+    public ScheduleModel from(ScheduleEntity scheduleEntity) {
+        return null;
+    }
+
+    @Override
+    public ScheduleEntity toEntity(ScheduleModel source) {
+        return ScheduleEntity.builder()
+                .eventId(source.getId())
+                .eventName(source.getEventName())
+                .eventStartDate(source.getEventStartDate())
+                .eventEndDate(source.getEventEndDate())
+                .eventPlace(source.getEventPlace())
+                .eventInfo(source.getEventInfo())
+                .build();
+    }
+}
