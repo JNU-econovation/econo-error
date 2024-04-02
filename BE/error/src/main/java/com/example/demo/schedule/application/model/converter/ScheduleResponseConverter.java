d147909495e4587ce4479d2105b9c3adfba52d9c
capDoYeonLee <ghtyu21@naver.com>
2024-04-02 11:46:50 +0900

    feat: 년도 조회 toModel method 기능 개발

--- BE/error/src/main/java/com/example/demo/schedule/application/model/converter/ScheduleResponseConverter.java
+++ BE/error/src/main/java/com/example/demo/schedule/application/model/converter/ScheduleResponseConverter.java
@@ -2,11 +2,14 @@ package com.example.demo.schedule.application.model.converter;
 
 import com.example.demo.schedule.application.dto.CreateScheduleResponse;
 import com.example.demo.schedule.application.dto.SpecificScheduleResopnse;
+import com.example.demo.schedule.application.dto.YearCalendarResponse;
 import com.example.demo.schedule.application.model.ScheduleModel;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ScheduleResponseConverter {
@@ -17,6 +20,7 @@ public class ScheduleResponseConverter {
         return CreateScheduleResponse.builder().eventId(eventId).build();
     }
 
+
     public SpecificScheduleResopnse from(ScheduleModel model) {
         return SpecificScheduleResopnse.builder()
                 .eventId(model.getEventId())
@@ -26,6 +30,20 @@ public class ScheduleResponseConverter {
                 .eventInfo(model.getEventInfo())
                 .eventPlace(model.getEventPlace())
                 .build();
+    }
 
+    public List<YearCalendarResponse> toModel(List<ScheduleModel> models) {
+
+        List<YearCalendarResponse> response = new ArrayList<>();
+        for (ScheduleModel model : models) {
+            YearCalendarResponse schedule = YearCalendarResponse.builder()
+                    .eventId(model.getEventId())
+                    .eventStartDate(String.valueOf(LocalDateTime.parse(model.getEventStartDate().toString(), formatter)))
+                    .eventEndDate(String.valueOf(LocalDateTime.parse(model.getEventEndDate().toString(), formatter)))
+                    .build();
+            response.add(schedule);
+        }
+
+        return response;
     }
 }
