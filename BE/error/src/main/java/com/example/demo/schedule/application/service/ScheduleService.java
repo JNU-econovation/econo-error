71a25ccd2f695730497fca4108cba7f7540eefa3
capDoYeonLee <ghtyu21@naver.com>
2024-04-03 02:13:15 +0900

    feat: 주별 조회 service 로직 기능 개발

--- BE/error/src/main/java/com/example/demo/schedule/application/service/ScheduleService.java
+++ BE/error/src/main/java/com/example/demo/schedule/application/service/ScheduleService.java
@@ -12,6 +12,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -25,7 +29,8 @@ public class ScheduleService implements CreateScheduleUsecase,
                                         GetYearScheduleUsecase,
                                         GetMonthScheduleUsecase,
                                         UpdateScheduleUsecase,
-                                        DeleteScheduleUsecase {
+                                        DeleteScheduleUsecase,
+                                        GetWeekScheduleUsecase {
 
     private final ScheduleRequestConverter requestConverter;
     private final ScheduleEntityConverter entityConverter;
@@ -107,7 +112,6 @@ public class ScheduleService implements CreateScheduleUsecase,
     }
 
 
-
     @Override
     public List<MonthCalendarResponse> getMonthSchedule(final int year, final int month) {
         List<ScheduleModel> model = filterEntitiesByMonth(year, month);
@@ -121,6 +125,25 @@ public class ScheduleService implements CreateScheduleUsecase,
                 .map(entityConverter::from)
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public List<WeekCalendarResponse> getWeekSchedule(final int year, final int month, final int day) {
+        LocalDate wantFindWeek = LocalDate.of(year, month, day);
+        List<ScheduleModel> model = filterEntitiesByWeek(wantFindWeek);
+
+        return responseConverter.toWeekModel(model);
+    }
+
+    private List<ScheduleModel> filterEntitiesByWeek(final LocalDate wantFindWeek) {
+
+        Stream<ScheduleEntity> entities = scheduleRepository.streamAll();
+        final int weekOfYear = wantFindWeek.get(WeekFields.ISO.weekOfWeekBasedYear());
+        return entities
+                .filter(entity -> entity.getEventStartDate().toLocalDate().get(WeekFields.ISO.weekOfWeekBasedYear()) == weekOfYear)
+                .map(entityConverter::from)
+                .collect(Collectors.toList());
+    }
+
 
 
 
