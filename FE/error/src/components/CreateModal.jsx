3e5f5e085a25487904baa1a568a2670ef7aeefd4
mlnwns <mlnwns@naver.com>
2024-04-03 19:54:46 +0900

    chore: DayPicker 라이브러리 관련 내용 삭제

--- FE/error/src/components/CreateModal.jsx
+++ FE/error/src/components/CreateModal.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react"; // useState 추가
 import Modal from "react-modal";
 import "./CreateModal.css";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { format } from "date-fns"; // format 함수 추가
 
 const CreateModal = ({ isOpen, onRequestClose }) => {
   return (
@@ -11,11 +14,10 @@ const CreateModal = ({ isOpen, onRequestClose }) => {
       overlayClassName="overlay"
     >
       <input placeholder="제목" />
-
-      <input type="date" id="date" />
-      <input type="date" id="date" />
-      <input type="time" />
-      <input type="time" />
+      <input type="date" id="date1" /> {/* id 수정 */}
+      <input type="date" id="date2" /> {/* id 수정 */}
+      <input type="time" id="time1" /> {/* id 추가 */}
+      <input type="time" id="time2" /> {/* id 추가 */}
       <p>모달 내용</p>
       <button onClick={onRequestClose}>저장</button>
     </Modal>
