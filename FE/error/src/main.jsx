481a1cc711203697be1d0560bc8f228a84b70cde
hai <xgkg0344@naver.com>
2024-04-04 21:29:17 +0900

    feat: Modal import

--- FE/error/src/main.jsx
+++ FE/error/src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Modal from "react-modal";
 import App from "./App.jsx";
 import "./fonts/font.css";
+
+const rootElement = document.getElementById("root");
+Modal.setAppElement(rootElement);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
