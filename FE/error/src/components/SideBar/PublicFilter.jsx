85401c458d4af6ac1d75a8c6133dd12c94b234ea
hai <xgkg0344@naver.com>
2024-05-22 23:50:48 +0900

    feat: 스케줄 체크박스 추가

--- FE/error/src/components/SideBar/PublicFilter.jsx
+++ FE/error/src/components/SideBar/PublicFilter.jsx
@@ -1,36 +1,25 @@
 import styled from "styled-components";
-import { FaCheck } from "react-icons/fa";
-
-import { useState } from "react";
+import ScheduleToggle from "../ScheduleToggle";
 
 const PublicFilter = () => {
-  const [publicClicked, setPublicClicked] = useState(false);
-  const [WeeklyClicked, setWeeklyClicked] = useState(false);
-  const handlePublicClick = () => {
-    if (publicClicked === false) setPublicClicked(true);
-    else setPublicClicked(false);
-  };
-  const handleWeeklyClick = () => {
-    if (WeeklyClicked === false) setWeeklyClicked(true);
-    else setWeeklyClicked(false);
-  };
   return (
     <PublicFilterFrame>
-      <div style={{ color: "rgb(60, 64, 67)", marginBottom: "0.5rem" }}>
+      <div
+        style={{
+          color: "rgb(60, 64, 67)",
+          marginBottom: "0.5rem",
+        }}
+      >
         에코노 캘린더
       </div>
-      <div>
-        <PublicBtn clicked={publicClicked} onClick={handlePublicClick}>
-          <FaCheck size="0.6rem" />
-        </PublicBtn>
+      <SceduleType>
+        <ScheduleToggle color={"#ff5b5b"} />
         공식행사
-      </div>
-      <div>
-        <WeeklyBtn clicked={WeeklyClicked} onClick={handleWeeklyClick}>
-          <FaCheck size="0.6rem" />
-        </WeeklyBtn>
+      </SceduleType>
+      <SceduleType>
+        <ScheduleToggle color={"#63ABFF"} />
         주간발표
-      </div>
+      </SceduleType>
     </PublicFilterFrame>
   );
 };
@@ -40,31 +29,8 @@ export default PublicFilter;
 const PublicFilterFrame = styled.div`
   margin: 1.3rem;
 `;
-const PublicBtn = styled.button`
-  border: none;
-  background-color: #ff5b5b;
-  margin: 0.4rem;
-  color: white;
-  border: 0.15rem solid #ff5b5b;
-  border-radius: 0.15rem;
-  ${({ clicked }) =>
-    clicked &&
-    `background-color: transparent;
-    color: transparent;
-    border: 0.15em solid #ff5b5b;
-  `}
-`;
-const WeeklyBtn = styled.button`
-  border: none;
-  background-color: #63abff;
-  border: 0.15em solid #63abff;
-  color: aliceblue;
-  border-radius: 0.15rem;
-  margin: 0.4rem;
-  ${({ clicked }) =>
-    clicked &&
-    `background-color: transparent;
-    color: transparent;
-    border: 0.15em solid #63abff;
-  `}
+
+const SceduleType = styled.div`
+  display: flex;
+  align-items: center;
 `;
