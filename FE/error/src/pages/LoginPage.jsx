fe62340dd170a23ffb30c31e465ba233ec48d7e1
kanghaeun <xgkg0330@jnu.ac.kr>
2024-05-28 20:42:16 +0900

    feat: 로그인 버튼 클릭시 slack으로 이동하는 로직 추가

--- FE/error/src/pages/LoginPage.jsx
+++ FE/error/src/pages/LoginPage.jsx
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
 const LoginPage = () => {
+  const handleOnLogin = () => {
+    /* const clientId = "437291124342.7141431332214";
+    const redirectUri = encodeURIComponent(
+      "&state=&granular_bot_scope=0&single_channel=0&install_redirect=&tracked=1&team="
+    );
+    const scopes = encodeURIComponent("incoming-webhook&user_scope=");*/
+    const slackAuthUrl = `https://econovation-2018.slack.com/oauth?client_id=437291124342.7141431332214&scope=incoming-webhook&user_scope=&redirect_uri=&state=&granular_bot_scope=0&single_channel=0&install_redirect=&tracked=1&team=`;
+    window.location.href = slackAuthUrl;
+  };
   return (
     <>
       <StyledTextArea>
@@ -13,7 +22,7 @@ const LoginPage = () => {
           <br />
           공식 일정만 조회 가능합니다.
         </StyledSubTitle>
-        <StyledSlackButton>
+        <StyledSlackButton onClick={handleOnLogin}>
           <StyledSlackImage src="Slack.png"></StyledSlackImage>슬랙으로 로그인
         </StyledSlackButton>
       </StyledTextArea>
@@ -27,8 +36,8 @@ export default LoginPage;
 
 const StyledTextArea = styled.div`
   position: absolute;
-  top: 10rem;
-  left: 11rem;
+  top: 20%;
+  left: 15%;
 
   h2 {
     font-family: "Pretendard-bold";
@@ -65,13 +74,13 @@ const StyledSubTitle = styled.h3`
 `;
 
 const StyledBackground = styled.img`
-  margin-left: 40rem;
-  margin-top: 5rem;
+  margin-left: 48%;
+  margin-top: 8%;
   height: 35rem;
 `;
 
 const StyledCharacter = styled.img`
   position: absolute;
-  top: 13rem;
-  left: 55rem;
+  top: 25%;
+  left: 60%;
 `;
