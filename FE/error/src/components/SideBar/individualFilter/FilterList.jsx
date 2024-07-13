d832a6c09b17b3b1dfdbfb858f07d3b50cfcada5
kanghaeun <xgkg0330@jnu.ac.kr>
2024-07-13 18:13:01 +0900

    feat: 색상 및 제목 필터 리스트에 나열 기능 추가

---
+++ FE/error/src/components/SideBar/individualFilter/FilterList.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import ScheduleToggle from "../ScheduleToggle";
+const FilterList = ({ filterLists }) => {
+  return (
+    <StyledFilterListFrame>
+      {filterLists.map((filterList, index) => (
+        <StyledSceduleType key={index}>
+          <ScheduleToggle color={filterList.color} />
+          {filterList.title}
+        </StyledSceduleType>
+      ))}
+    </StyledFilterListFrame>
+  );
+};
+
+export default FilterList;
+
+const StyledFilterListFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1.3rem;
+  margin-bottom: 0.3rem;
+`;
+const StyledSceduleType = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.3rem;
+`;
