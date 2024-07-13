93effd613f8e82daa3364e81d89d6658d6dba36e
kanghaeun <xgkg0330@jnu.ac.kr>
2024-07-13 18:05:15 +0900

    feat: 필터 제목 및 색상 상태 추가

--- FE/error/src/components/SideBar/individualFilter/IndividualFilter.jsx
+++ FE/error/src/components/SideBar/individualFilter/IndividualFilter.jsx
@@ -3,37 +3,45 @@ import { FaPlus } from "react-icons/fa6";
 import { SlArrowDown } from "react-icons/sl";
 import styled from "styled-components";
 import FilterCreateModal from "./FilterCreateModal";
+import FilterList from "./FilterList";
 
 const IndividualFilter = () => {
   const [individualFilterIsOpen, setindividualFilterIsOpen] = useState(false);
+  const [filterLists, setFilterLists] = useState([]);
 
   const createIndividualFilter = () => {
     setindividualFilterIsOpen(true);
   };
 
+  const addNewFilter = (newFilter) => {
+    setFilterLists([...filterLists, newFilter]);
+  };
   return (
-    <StyledIndividualFilterFrame>
-      <StyledTextContainer>
-        <span
-          style={{
-            color: "#333333",
-            marginBottom: "0.5rem",
-            fontSize: "1.1rem",
-          }}
-        >
-          개인 캘린더
-        </span>
-      </StyledTextContainer>
-      <StyledIndividualFilterPlusBtn onClick={createIndividualFilter}>
-        <FaPlus />
-        <SlArrowDown style={{ fontWeight: "bold", marginLeft: "0.5rem" }} />
-      </StyledIndividualFilterPlusBtn>
-      <FilterCreateModal
-        isOpen={individualFilterIsOpen}
-        onRequestClose={() => setindividualFilterIsOpen(false)}
-        filterModalType={"individual"}
-      />
-    </StyledIndividualFilterFrame>
+    <>
+      <StyledIndividualFilterFrame>
+        <StyledTextContainer>
+          <span
+            style={{
+              color: "#333333",
+              fontSize: "1.1rem",
+            }}
+          >
+            개인 캘린더
+          </span>
+        </StyledTextContainer>
+        <StyledIndividualFilterPlusBtn onClick={createIndividualFilter}>
+          <FaPlus />
+          <SlArrowDown style={{ fontWeight: "bold", marginLeft: "0.5rem" }} />
+        </StyledIndividualFilterPlusBtn>
+        <FilterCreateModal
+          isOpen={individualFilterIsOpen}
+          onRequestClose={() => setindividualFilterIsOpen(false)}
+          filterModalType={"individual"}
+          addNewFilter={addNewFilter}
+        />
+      </StyledIndividualFilterFrame>
+      <FilterList filterLists={filterLists} />
+    </>
   );
 };
 
