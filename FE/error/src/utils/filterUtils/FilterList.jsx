834495c2fb08e47cbab5f1f806673b64fbfd7a04
kanghaeun <xgkg0330@jnu.ac.kr>
2024-07-16 01:10:51 +0900

    style: 필터 삭제 및 편집 아이콘 마우스 호버기능 추가

--- FE/error/src/utils/filterUtils/FilterList.jsx
+++ FE/error/src/utils/filterUtils/FilterList.jsx
@@ -1,12 +1,35 @@
+import { AiOutlineClose, AiOutlineMore } from "react-icons/ai";
 import styled from "styled-components";
 import ScheduleToggle from "./ScheduleToggle";
+
 const FilterList = ({ filterLists }) => {
+  const filterDelete = () => {};
+
   return (
     <StyledFilterListFrame>
       {filterLists.map((filterList, index) => (
         <StyledSceduleType key={index}>
-          <ScheduleToggle color={filterList.color} />
-          {filterList.title}
+          <StyledTitle>
+            <ScheduleToggle color={filterList.color} />
+            {filterList.title}
+          </StyledTitle>
+          <StyledDetail>
+            <StyledDetailIcon
+              onClick={filterDelete}
+              className="detail-icon"
+              onMouseEnter={(e) => (e.currentTarget.style.color = "#000")}
+              onMouseLeave={(e) => (e.currentTarget.style.color = "#B8B6B6")}
+            >
+              <AiOutlineClose size="1.1rem" />
+            </StyledDetailIcon>
+            <StyledDetailIcon
+              className="detail-icon"
+              onMouseEnter={(e) => (e.currentTarget.style.color = "#000")}
+              onMouseLeave={(e) => (e.currentTarget.style.color = "#B8B6B6")}
+            >
+              <AiOutlineMore size="1.4rem" />
+            </StyledDetailIcon>
+          </StyledDetail>
         </StyledSceduleType>
       ))}
     </StyledFilterListFrame>
@@ -22,7 +45,26 @@ const StyledFilterListFrame = styled.div`
   margin-bottom: 0.3rem;
 `;
 const StyledSceduleType = styled.div`
+  height: 1.8rem;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   margin-bottom: 0.3rem;
+  &:hover .detail-icon {
+    display: block;
+  }
+`;
+const StyledDetail = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 1rem;
+`;
+const StyledTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const StyledDetailIcon = styled.button`
+  background-color: #fff;
+  border: none;
+  display: none;
 `;
