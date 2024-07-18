87b3df834c8fddee18f5311c126e50232e53a5c2
kanghaeun <xgkg0330@jnu.ac.kr>
2024-07-19 00:52:02 +0900

    feat: 조회컴포넌트를 Details와 Actions 컴포넌트로 분리

---
+++ FE/error/src/components/scheduleCheck/EventActions.jsx
@@ -0,0 +1,7 @@
+const EventActions = () => {
+  // 일정 수정, 삭제 등의 액션을 처리하는 컴포넌트
+  // selectID, events, handleDelete, handleUpdateDeleteData를 props로 받아와서 처리
+  return <div></div>;
+};
+
+export default EventActions;
---
+++ FE/error/src/components/scheduleCheck/EventDetails.jsx
@@ -0,0 +1,7 @@
+const EventDetails = () => {
+  //일정 상세 정보를 표시하는 컴포넌트
+  // event 객체를 props로 받아와 렌더링
+  return <div></div>;
+};
+
+export default EventDetails;
