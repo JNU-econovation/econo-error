d26138660a907a23736425cf9f4a0df88e4042bf
SeoYeongU <westzeroright@gmail.com>
2024-07-23 14:47:45 +0900

    refactor: url 리소스 복수형으로 표기

--- BE/error/src/main/java/com/example/demo/image/ImageController.java
+++ BE/error/src/main/java/com/example/demo/image/ImageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/image")
+@RequestMapping("/api/images")
 public class ImageController {
 
     private final ImageService imageService;
