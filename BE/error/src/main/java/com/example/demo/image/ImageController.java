7bc6658e49444d37326eb5ce4247e973138e2635
SeoYeongU <westzeroright@gmail.com>
2024-07-23 13:48:44 +0900

    refactor: id와 image url을 함께 반환

--- BE/error/src/main/java/com/example/demo/image/ImageController.java
+++ BE/error/src/main/java/com/example/demo/image/ImageController.java
@@ -1,6 +1,7 @@
 package com.example.demo.image;
 
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +20,8 @@ public class ImageController {
     }
 
     @GetMapping
-    public List<String> getProfileImages (){
-        List<String> profileImageList = imageService.getFileList("image0");
+    public List<Map<String, Object>> getProfileImages (){
+        List<Map<String, Object>> profileImageList = imageService.getFileList("styleganresult2");
         return profileImageList;
     }
-
 }
--- BE/error/src/main/java/com/example/demo/image/ImageService.java
+++ BE/error/src/main/java/com/example/demo/image/ImageService.java
@@ -6,7 +6,9 @@ import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,20 +27,24 @@ public class ImageService {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    public List<String> getFileList(String directory) {
-        List<String> fileList = new ArrayList<>();
+    public List<Map<String, Object>> getFileList(String directory) {
+        List<Map<String, Object>> fileList = new ArrayList<>();
 
         ListObjectsV2Request listObjectsV2Request = new ListObjectsV2Request()
             .withBucketName(bucketName)
-            .withPrefix(directory+"/");
+            .withPrefix(directory + "/");
 
         ListObjectsV2Result result = amazonS3Client.listObjectsV2(listObjectsV2Request);
         List<S3ObjectSummary> objectSummaries = result.getObjectSummaries();
 
+        int id = 1;
         for (S3ObjectSummary objectSummary : objectSummaries) {
             String key = objectSummary.getKey();
             if (!key.equals(directory + "/")) {
-                fileList.add("https://"+bucketName+".s3."+region+".amazonaws.com/" + key);
+                Map<String, Object> fileInfo = new HashMap<>();
+                fileInfo.put("id", id++);
+                fileInfo.put("url", "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key);
+                fileList.add(fileInfo);
             }
         }
 
