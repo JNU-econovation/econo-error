5b0301ca3570ec79885169acaa4fe40d343ab0c7
capDoYeonLee <ghtyu21@naver.com>
2024-07-24 17:30:26 +0900

    feat: private, public dto 추가

---
+++ BE/error/src/main/java/com/example/demo/schedule/application/dto/AllPublicCalendarResponse.java
@@ -0,0 +1,23 @@
+package com.example.demo.schedule.application.dto;
+
+import com.example.demo.filter.persistence.FilterEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class AllPublicCalendarResponse {
+    private Long eventId;
+    private String eventName;
+    private String eventStartDate;
+    private String eventEndDate;
+    private String eventPlace;
+    private String eventInfo;
+    private String scheduleType;
+    private String filterName;
+    private String filterColor;
+}
