e249b3da5df9939c9e94765aac465e9e41f38b6d
capDoYeonLee <ghtyu21@naver.com>
2024-10-01 23:44:49 +0900

    refactor: ScheduleRepo 추상메서드 정의 및 jpa 상속 제거

--- BE/error/src/main/java/com/example/demo/schedule/domain/ScheduleRepository.java
+++ BE/error/src/main/java/com/example/demo/schedule/domain/ScheduleRepository.java
@@ -1,27 +1,21 @@
-package com.example.demo.schedule.persistence;
+package com.example.demo.schedule.domain;
 
-import com.example.demo.schedule.application.model.ScheduleModel;
-import jakarta.persistence.Entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
+import com.example.demo.schedule.infrastructure.persistence.ScheduleEntity;
+
+import java.util.Optional;
 import java.util.stream.Stream;
-//import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
-    @Query("select p from ScheduleEntity p") Stream<ScheduleEntity> streamAll();
 
-    @Query("SELECT s FROM ScheduleEntity s WHERE s.scheduleType = 'PUBLIC'")
+public interface ScheduleRepository {
+
+    ScheduleEntity save(ScheduleEntity entity);
+
     Stream<ScheduleEntity> streamAllPublic();
 
-    @Query("SELECT s FROM ScheduleEntity s WHERE s.scheduleType = 'PRIVATE'")
     Stream<ScheduleEntity> streamAllPrivate();
 
-    @Query("SELECT p.eventId, p.eventName, p.eventStartDate, p.createdDate FROM ScheduleEntity p")
-    List<ScheduleModel> getYearCalendar();
+    Optional<ScheduleEntity> findById(Long eventId);
+
+    void deleteById(Long eventId);
 }
