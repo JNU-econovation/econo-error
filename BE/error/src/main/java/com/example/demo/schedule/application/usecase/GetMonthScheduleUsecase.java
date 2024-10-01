527bfc4f863a1f893a34949bf3f9234374a9cb8d
capDoYeonLee <ghtyu21@naver.com>
2024-10-01 19:37:49 +0900

    remove: usecase 삭제

--- BE/error/src/main/java/com/example/demo/schedule/application/usecase/GetMonthScheduleUsecase.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.schedule.application.usecase;
-
-import com.example.demo.schedule.application.dto.MonthCalendarResponse;
-
-import java.util.List;
-
-public interface GetMonthScheduleUsecase {
-    List<MonthCalendarResponse> getMonthSchedule(int year, int month);
-}
--- BE/error/src/main/java/com/example/demo/schedule/application/usecase/GetYearScheduleUsecase.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.schedule.application.usecase;
-
-import com.example.demo.schedule.application.dto.YearCalendarResponse;
-
-import java.util.List;
-
-public interface GetYearScheduleUsecase {
-    List<YearCalendarResponse> getYearSchedule(int year);
-}
