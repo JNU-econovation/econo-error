9958240746fbf60d0886cfb08ab216a19a5eb287
capDoYeonLee <ghtyu21@naver.com>
2024-10-01 23:42:30 +0900

    fix: 패키지 경로 수정

--- BE/error/src/main/java/com/example/demo/filter/application/model/converter/FilterRequestConverter.java
+++ BE/error/src/main/java/com/example/demo/filter/application/model/converter/FilterRequestConverter.java
@@ -2,10 +2,8 @@ package com.example.demo.filter.application.model.converter;
 
 import com.example.demo.common.support.converter.AbstractDtoConverter;
 import com.example.demo.filter.application.dto.CreateFilterRequest;
-import com.example.demo.filter.application.dto.CreateFilterResponse;
 import com.example.demo.filter.application.dto.UpdateFilterRequest;
 import com.example.demo.filter.application.model.FilterModel;
-import com.example.demo.schedule.application.model.ScheduleModel;
 import org.springframework.stereotype.Component;
 
 @Component
