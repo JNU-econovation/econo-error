e6f053cf30d3a206b05977c0813812ba3906a978
capDoYeonLee <ghtyu21@naver.com>
2024-10-03 22:33:55 +0900

    feat: filter 패키지 domain 계층 path 수정

--- BE/error/src/main/java/com/example/demo/filter/persistence/FilterRepository.java
+++ BE/error/src/main/java/com/example/demo/filter/persistence/FilterRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.filter.persistence;
 
-import com.example.demo.schedule.persistence.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
