4cfe97d5275c92bcd26b6af63d2cd53797307c7b
capDoYeonLee <ghtyu21@naver.com>
2024-10-08 20:44:46 +0900

    feat: default url 설정

--- BE/error/src/main/java/com/example/demo/ErrorApplication.java
+++ BE/error/src/main/java/com/example/demo/ErrorApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
@@ -14,6 +16,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients(basePackages = "com.example.demo.auth.infra.oauth.slack.client")
+@OpenAPIDefinition(
+		servers = {
+				@Server(url="https://error.econo-calendar.com", description = "Default Server url")
+		}
+)
 public class ErrorApplication {
 
 	public static void main(String[] args) {
